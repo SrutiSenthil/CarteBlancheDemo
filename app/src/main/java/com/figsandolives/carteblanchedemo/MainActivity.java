574cfd78108e0417361d8771f48955b446c31e38
rahim <abdurrahim.ceg@gmail.com>
2017-02-16 07:07:10 +0530

    Activity life cycle methods

--- app/src/main/java/com/figsandolives/carteblanchedemo/MainActivity.java
+++ app/src/main/java/com/figsandolives/carteblanchedemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.figsandolives.carteblanchedemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.e("CB", "In onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.e("CB", "In onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.e("CB", "In onStop");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.e("CB", "In onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.e("CB", "In onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Log.e("CB", "In onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.e("CB", "In onDestroy");
     }
 }
